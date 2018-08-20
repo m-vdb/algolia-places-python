cb49149a73a6fa38f98de5d89744d1b8cd0cf9fb
m-vdb <mvergerdelbove@work4labs.com>
2018-08-20 13:04:22 +0200

    add tests for BaseObject

--- algolia_places/struct.py
+++ algolia_places/struct.py
@@ -7,6 +7,9 @@ class BaseObject:
     """
     DIRECT_LOOKUPS = []
 
+    def __init__(self, data):
+        self.data = data
+
     def __dir__(self):
         """
         Give nice output to dir() calls.
@@ -16,9 +19,9 @@ class BaseObject:
     def __getattribute__(self, attr):
         """
         All attributes in `DIRECT_LOOKUPS` can be used directly
-        on the class instead of using the underlying `hit` dict.
+        on the class instead of using the underlying `data` dict.
         """
-        if attr in self.DIRECT_LOOKUPS:
-            return self.hit[attr]
+        if attr == 'DIRECT_LOOKUPS' or attr not in self.DIRECT_LOOKUPS:
+            return super().__getattribute__(attr)
 
-        return super().__getattribute__(attr)
+        return self.data[attr]
---
+++ tests/test_struct.py
@@ -0,0 +1,66 @@
+import unittest
+
+
+from algolia_places.struct import BaseObject
+
+
+class MyObject(BaseObject):
+
+    DIRECT_LOOKUPS = ['attr1', 'attr2']
+
+
+class BaseObjectTestCase(unittest.TestCase):
+
+    def setUp(self):
+        super().setUp()
+        self.obj = MyObject({'attr1': 'yeah', 'attr2': 'boom'})
+
+    def test_init(self):
+        self.assertEqual(
+            self.obj.data,
+            {'attr1': 'yeah', 'attr2': 'boom'}
+        )
+
+    def test_getattribute(self):
+        self.assertEqual(self.obj.attr1, 'yeah')
+        self.assertEqual(self.obj.attr2, 'boom')
+        with self.assertRaises(AttributeError):
+            self.obj.attr3
+
+    def test_dir(self):
+        self.assertEqual(
+            dir(self.obj),
+            # sorted
+            [
+                'DIRECT_LOOKUPS',
+                '__class__',
+                '__delattr__',
+                '__dict__',
+                '__dir__',
+                '__doc__',
+                '__eq__',
+                '__format__',
+                '__ge__',
+                '__getattribute__',
+                '__gt__',
+                '__hash__',
+                '__init__',
+                '__init_subclass__',
+                '__le__',
+                '__lt__',
+                '__module__',
+                '__ne__',
+                '__new__',
+                '__reduce__',
+                '__reduce_ex__',
+                '__repr__',
+                '__setattr__',
+                '__sizeof__',
+                '__str__',
+                '__subclasshook__',
+                '__weakref__',
+                'attr1',
+                'attr2',
+                'data',
+            ]
+        )
